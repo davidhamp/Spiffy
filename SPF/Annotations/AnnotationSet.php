40a91505ebc3fb5b676098f1c0644fa292be903d
David Hamp <david.hamp@gmail.com>
2015-09-24 11:01:46 -0700

    Documentation update

--- SPF/Annotations/AnnotationSet.php
+++ SPF/Annotations/AnnotationSet.php
@@ -10,15 +10,39 @@
 namespace SPF\Annotations;
 
 /**
+ * Annotation container always returned by {@link SPF\Annotations\Engine::get()}
+ * 
  * @todo Update to use Iterator for traversing over annotations
  */
 class AnnotationSet
 {
-
+    /**
+     * Collection of annotation information keyed by Annotation name
+     * 
+     * @var array
+     */
     protected $annotations = array();
 
+    /**
+     * The annotation set reflects a group of annotations present in a given element of an object
+     * 
+     * For convenience and for validation against things like parameters of method or visibility we provide
+     * the reflection object here.  Note this isn't the full reflectionClass, but either an instance of 
+     * ReflectionMethod or ReflectionProperty depending on the type of element requested through
+     * {@link SPF\Annotations\Engine::get()}
+     * 
+     * @var ReflectionMethod|ReflectionProperty
+     */
     public $reflection;
 
+    /**
+     * Recieves the annotation set as an array and the reflection object
+     *
+     * @param array                               $annotations Array of matched annotaions
+     * @param ReflectionMethod|ReflectionProperty $reflection Reflection element instance stored for reference
+     *
+     * @return void
+     */
     public function __construct($annotations = array(), $reflection = null)
     {
         $this->reflection = $reflection;
@@ -35,11 +59,23 @@ class AnnotationSet
         }
     }
 
+    /**
+     * Helper function to determine existence of Annotations in the set
+     * 
+     * @return boolean
+     */
     public function has($annotation)
     {
         return array_key_exists($annotation, $this->annotations);
     }
 
+    /**
+     * Returns all annotations or a subset of annotations based on annotation name
+     * 
+     * @param string $annotation Name of annotation to retrieve from the set.  Will return all annotations if set to null
+     * 
+     * @return array
+     */
     public function get($annotation = null)
     {
         if (is_null($annotation)) {
